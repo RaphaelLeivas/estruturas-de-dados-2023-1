7dc794e4adfd6eec6877b225b055454ed2eea702
RaphaelLeivas <raphaelhenrique2013@outlook.com>
2023-04-14 11:35:25 -0300

    add scrpit e make de debug para AP04

---
+++ AP04/compileDebug.sh
@@ -0,0 +1,6 @@
+echo "Cleaning make..."
+make clean
+echo "Make successfully cleaned. Running in dev mode..."
+make dev
+echo "Compile in dev successfull."
+gdb /mnt/c/dev/estruturas-de-dados-2023-1/ap04/bin/AP04
