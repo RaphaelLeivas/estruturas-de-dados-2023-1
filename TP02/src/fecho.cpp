09e4da680638eb2aa5246b4c9e92a2d3077a341d
RaphaelLeivas <raphaelhenrique2013@outlook.com>
2023-06-09 20:40:47 -0300

    arquivos base TP02

---
+++ TP02/src/fecho.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <iostream>
+
+#include "../include/Stack.hpp"
+
+#define STACK_SIZE 10
+
+int main(int argc, char** argv) {
+    int num = 100;
+
+    std::cout << num << std::endl;
+
+    return 0;
+}
