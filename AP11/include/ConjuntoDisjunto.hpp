f44108927e648108c680133a5fdb66f2c2d6d2d1
RaphaelLeivas <raphaelhenrique2013@outlook.com>
2023-06-25 20:28:57 -0300

    codigo AP 11 estável

--- AP11/include/ConjuntoDisjunto.hpp
+++ AP11/include/ConjuntoDisjunto.hpp
@@ -1,12 +1,18 @@
+#ifndef CONJUNTO_DISJUNTO_HPP
+#define CONJUNTO_DISJUNTO_HPP
+
+#define MAX_TAM 10000
 
 class ConjuntoDisjunto {
-private:
-	
+   private:
+	long parent[MAX_TAM];
 
-public:	
-	ConjuntoDisjunto();
-	~ConjuntoDisjunto();
-	void makeSet(long x);
-	void unionSet(long x, long y);
-	long findSet(long x);
+   public:
+    ConjuntoDisjunto();
+    ~ConjuntoDisjunto();
+    void makeSet(long x);
+    void unionSet(long x, long y);
+    long findSet(long x);
 };
+
+#endif  // CONJUNTO_DISJUNTO_HPP
--- AP11/src/ConjuntoDisjunto.cpp
+++ AP11/src/ConjuntoDisjunto.cpp
@@ -1,10 +1,25 @@
-#include "ConjuntoDisjunto.hpp" 
+#include "ConjuntoDisjunto.hpp"
 
-	
-ConjuntoDisjunto::ConjuntoDisjunto(){}
-ConjuntoDisjunto::~ConjuntoDisjunto(){}
+// defaults
+ConjuntoDisjunto::ConjuntoDisjunto() {}
+ConjuntoDisjunto::~ConjuntoDisjunto() {}
 
-void ConjuntoDisjunto::makeSet(long x){}
-void ConjuntoDisjunto::unionSet(long x, long y){}
-long ConjuntoDisjunto::findSet(long x){return 0;}
+void ConjuntoDisjunto::makeSet(long x) {
+    // cria um novo conjunto cujo único membro é x
+    parent[x] = x;
+}
 
+void ConjuntoDisjunto::unionSet(long x, long y) {
+    // une os conjuntos dinâmicos que contêm x e y, digamos Sx e Sy, em um novo
+    // conjunto que é a união desses dois conjuntos
+    int m = this->findSet(x);
+    int n = this->findSet(y);
+    parent[m] = n;
+}
+
+long ConjuntoDisjunto::findSet(long x) {
+    // retorna um ponteiro para o representante do (único) conjunto que contém x
+    if (parent[x] == x)  
+        return x;
+    return this->findSet(parent[x]);  
+}
