89fccf6c77de8c49368a5dbbb699b37208c49be0
RaphaelLeivas <raphaelhenrique2013@outlook.com>
2023-06-29 09:22:18 -0300

    add base TP03

---
+++ TP03/src/TP03.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+// file management
+// #include <getopt.h>
+
+#include <fstream>
+
+std::string inputFilePath;
+
+// void parse_args(int argc, char** argv) {
+//     int c;
+//     while ((c = getopt(argc, argv, "f:")) != EOF) {
+//         if (c == 'f') {
+//             inputFilePath = optarg;
+//         }
+//     }
+// }
+
+int main(int argc, char** argv) {
+    // parse_args(argc, argv);
+
+    std::cout << "HELLO_VERSION" << std::endl;
+    return 0;
+}
